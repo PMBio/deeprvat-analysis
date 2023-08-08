8196946b15619cbf46bc26ee8a22bafcff100e64
Eva Holtkamp <eva.holtkamp@gmx.de>
2023-08-08 17:24:14 +0200

    updates to use additional phenotypes that have not been used for training

--- association_testing/compute_replication.py
+++ association_testing/compute_replication.py
@@ -11,12 +11,19 @@ import yaml
 from deeprvat.utils import pval_correction
 from tqdm import tqdm
 
-
 phenocode_df = pd.read_parquet("phenocodes.parquet", engine="pyarrow")
-phenocode_dict = {
-    pheno: int(code.split("-")[0])
-    for pheno, code in zip(phenocode_df["phenotype"], phenocode_df["phenocode"])
-}
+# phenocode_dict = {
+#     pheno: int(code.split("-")[0])
+#     for pheno, code in zip(phenocode_df["phenotype"], phenocode_df["phenocode"])
+# }
+phenocode_dict = {}
+for pheno, code in zip(phenocode_df['phenotype'], phenocode_df['phenocode']):
+    try:
+        code = int(code.split('-')[0])
+    except:
+        code = code
+    phenocode_dict[pheno] = code
+
 
 GENEBASS_NAME_DICT = copy.deepcopy(phenocode_dict)
 # GENEBASS_NAME_DICT.update({f"{k}_standardized": v for k, v in phenocode_dict.items()})
@@ -34,7 +41,7 @@ UKB_500_NAME_DICT.update(
     }
 )
 
-PHENOTYPES = [
+OLD_PHENOTYPES = [
     "Apolipoprotein_A",
     "Apolipoprotein_B",
     "Calcium",
@@ -57,6 +64,63 @@ PHENOTYPES = [
     "Neutrophill_count",
     "Red_blood_cell_erythrocyte_count",
 ]
+TRAINING_PHENOTYPES = copy.deepcopy(OLD_PHENOTYPES)
+
+# PHENOTYPES = [
+#     "Apolipoprotein_A",
+#     "Apolipoprotein_B",
+#     "Calcium",
+#     "Cholesterol",
+#     "HDL_cholesterol",
+#     "IGF_1",
+#     "LDL_direct",
+#     "SHBG",
+#     "Total_bilirubin",
+#     "Triglycerides",
+#     "Urate",
+#     "Mean_corpuscular_volume",
+#     "Platelet_count",
+#     "Mean_platelet_thrombocyte_volume",
+#     "Platelet_crit",
+#     "Standing_height",
+#     "Mean_reticulocyte_volume",
+#     "Platelet_distribution_width",
+#     "Lymphocyte_percentage",
+#     "Neutrophill_count",
+#     "Red_blood_cell_erythrocyte_count",
+#     "Body_mass_index_BMI",
+#     "Glucose",
+#     "Vitamin_D",
+#     "Albumin",
+#     "Total_protein",
+#     "Cystatin_C",
+#     "Gamma_glutamyltransferase",
+#     "Alkaline_phosphatase",
+#     "Creatinine"
+# ]
+
+NEW_PHENOTYPES = [
+    "Body_mass_index_BMI",
+    "Glucose",
+    "Vitamin_D",
+    "Albumin",
+    "Total_protein",
+    "Cystatin_C",
+    "Gamma_glutamyltransferase",
+    "Alkaline_phosphatase",
+    "Creatinine", 
+    "Whole_body_fat_free_mass",
+    "Forced_expiratory_volume_in_1_second_FEV1",
+    "Glycated_haemoglobin_HbA1c",
+    "QTC_interval",
+    'WHR',
+    'WHR_Body_mass_index_BMI_corrected' #not using these phenos because they are not in genebas/backman
+]
+
+# PHENOTYPES = NEW_PHENOTYPES
+PHENOTYPES = [*NEW_PHENOTYPES, *OLD_PHENOTYPES]
+# PHENOTYPES = OLD_PHENOTYPES
+print(f'analysing phenotypes {PHENOTYPES}')
 
 METHODS = list(
     reversed(
@@ -90,7 +154,6 @@ def read_comparison_results(comparison_dir: str, gene_df: pd.DataFrame, pheno: s
     # genebass_ensgids = genebass.loc[significant_mask, 'gene_id']
     genebass_ensgids = genebass["gene_id"]
     genebass_ids = gene_df.query("ensgid in @genebass_ensgids")["id"]
-
     backman = pd.read_excel(
         Path(comparison_dir) / "41586_2021_4103_MOESM5_ESM.xlsx",
         sheet_name="SD2",
@@ -140,7 +203,6 @@ def prep_for_rep_plot(
     #                                    ' and correction_method == "FDR"'
     #                                    ' and phenotype in @pheno_to_use'
     #                                   )
-
     replication_results["replicated"] = [
         gene in comparison_results[pheno]
         for pheno, gene in zip(
@@ -208,16 +270,31 @@ def cli(out_dir: str, experiment_dir: str, recompute_comparison_results: bool):
         with open("comparison_results.pkl", "rb") as f:
             comparison_results = pickle.load(f)
 
-    results = pd.concat(
-        [
-            pd.read_parquet(
+    result_list = []
+    for p in PHENOTYPES:
+        this_res = pd.read_parquet(
+               
                 Path(experiment_dir) / p / "deeprvat/eval/all_results.parquet",
                 engine="pyarrow",
             )
-            for p in PHENOTYPES
-        ]
-    )
-
+        if p in NEW_PHENOTYPES:
+            print(f'using DeepRVAT wo baseline counts for DeepRVAT for pheno {p}')
+            this_res = this_res.query('Method != "DeepRVAT"')
+            this_res.loc[this_res['Method'] == 'DeepRVAT wo baseline', 'Method'] = 'DeepRVAT'
+        result_list.append(this_res)
+    results = pd.concat(result_list)
+    phenotypes_to_remove = set(results['phenotype'].unique()) - set(comparison_results.keys())
+    print(f'excluding pheotypes {phenotypes_to_remove} because they are not in comparison studies')
+    results = results[~results['phenotype'].isin(phenotypes_to_remove)]
+    # results = pd.concat(
+    #     [
+    #         pd.read_parquet(
+    #             Path(experiment_dir) / p / "deeprvat/eval/all_results.parquet",
+    #             engine="pyarrow",
+    #         )
+    #         for p in PHENOTYPES
+    #     ]
+    # )
     rep_list = []
     replication_data_all = prep_for_rep_plot(
         results,
