87d6f373618d8384a8522b06b3227fd306afc65c
Eva Holtkamp <eva.holtkamp@gmx.de>
2023-10-09 14:27:42 +0200

    use top x discoveries if no discoveries are significant

--- phenotype_prediction/phenotype_prediction_pipeline/extract_burdens_for_prs.py
+++ phenotype_prediction/phenotype_prediction_pipeline/extract_burdens_for_prs.py
@@ -94,25 +94,59 @@ def extract_burdens(
     logger.info("succesfully completed")
 
 
+
 @cli.command()
 @click.option("--sig-file", type=click.Path(exists=True), multiple=True)
-@click.option("--train-pheno", multiple=True)
+@click.option("--min-discoveries", type=int, default=1)
 @click.argument("out-file", type=click.Path())
-def combine_significant(sig_file, train_pheno, out_file):
+def combine_significant(sig_file, min_discoveries, out_file):
+    methods_to_keep = ['DeepRVAT', 'Burden/SKAT combined']
+    phenotypes = [file.split('/')[2].replace('_', ' ') for file in sig_file]
+
+    logger.info(f'Combining discoveries for phenotypes {phenotypes}')
+    sig_file_dict = dict(zip(phenotypes, sig_file))
     all_discoveries = pd.concat([pd.read_parquet(file) for file in sig_file])
-    discovery_list = []
-    for pheno in all_discoveries['phenotype'].unique():
-        if pheno in train_pheno:
-            logger.info(f'Using DeepRVAT with seed genes as DeepRVAT for pheno {pheno}')
-            this_deeprvat = all_discoveries.query("phenotype == @pheno & Method == 'DeepRVAT'")
-        else:
-            logger.info(f'Using DeepRVAT without seed genes as DeepRVAT for pheno {pheno}')
-            this_deeprvat = all_discoveries.query("phenotype == @pheno & Method == 'DeepRVAT wo baseline'")
-            this_deeprvat['Method'] = 'DeepRVAT'
-        discovery_list.append(this_deeprvat)
-    baseline_discoveries = all_discoveries.query("Method not in ['DeepRVAT', 'DeepRVAT wo baseline']")
-    discovery_list.append(baseline_discoveries)
-    all_discoveries = pd.concat(discovery_list).rename(columns={'phenotype':'Trait'})
+    all_discoveries = all_discoveries.query("Method != 'DeepRVAT wo baseline'")
+    
+    existing_combinations = {(p, m) for p in phenotypes for m in methods_to_keep}
+    disc_counts = all_discoveries.query('Method in @methods_to_keep')\
+        .groupby(['phenotype', 'Method']).size()\
+        .reset_index()\
+        .rename(columns = {0: 'discoveries'})
+    logger.info(f'Significant discoveries {disc_counts}')
+    logger.info(f'Requiring at least {min_discoveries} discoveries. Taking genes with lowest pval of not enough genes are significant')
+
+    suff_counts = disc_counts.query('discoveries >= @min_discoveries')
+
+    existing_data_set = set(tuple(row) for row in suff_counts[['phenotype', 'Method']].to_numpy())
+    logger.info(f'Combinations with sufficient discoveries: {suff_counts}')
+    missing_combinations = existing_combinations - existing_data_set
+    missing_combinations = pd.DataFrame(missing_combinations, columns = ['phenotype', 'Method'])
+    assert len(missing_combinations) + len(suff_counts) == len(existing_combinations)
+
+    if min_discoveries == 0:
+        logger.info('Min discoveries is set to 0. Writing significant genes only.')
+        logger.info('Caution: The following phenotype/method combinations have zero discoveries')
+        logger.info(missing_combinations)
+    else:
+        all_discoveries = suff_counts[['phenotype', 'Method']].merge(all_discoveries, how = 'left')
+        assert len(all_discoveries[['phenotype', 'Method']].drop_duplicates()) == len(suff_counts)
+
+        for pheno in missing_combinations['phenotype'].unique():
+            logger.info(f'Adding missing discoveries for phenotype {pheno}')
+            missing_methods = missing_combinations.query('phenotype == @pheno')['Method'].unique()
+            all_pvals = pd.read_parquet(f"{os.path.dirname(sig_file_dict[pheno])}/all_results.parquet")
+            top_associations = all_pvals.query('Method in @missing_methods')\
+                .sort_values(['Method', 'pval'])\
+                .drop_duplicates(subset = ['phenotype', 'gene', 'Method'])\
+                .groupby('Method')\
+                .apply(lambda group: group.nsmallest(min_discoveries, 'pval'))\
+                .reset_index(drop=True)
+            all_discoveries = pd.concat([all_discoveries, top_associations])
+            
+        assert len(all_discoveries[['phenotype', 'Method']].drop_duplicates()) == len(existing_combinations)
+
+    all_discoveries = all_discoveries.rename(columns={'phenotype':'Trait'})
     this_pheno_mapping = dict(zip([i.replace('_', ' ') for i in PHENOTYPE_MAPPING.keys()], 
         PHENOTYPE_MAPPING.values()))
     all_discoveries['Trait'] =  all_discoveries["Trait"].replace(this_pheno_mapping)
