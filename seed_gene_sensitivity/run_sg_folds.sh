bb7a92846b21d05cb6e359b734cbdcdc9f6638bb
Meyer <k286r@odcf-worker02.inet.dkfz-heidelberg.de>
2023-11-17 09:16:57 +0100

    Fix directory paths

--- seed_gene_sensitivity/run_sg_folds.sh
+++ seed_gene_sensitivity/run_sg_folds.sh
@@ -5,31 +5,32 @@
 # Activate your virt-env before running script. i.e. $ mamba activate deeprvat
 
 FOLDS=5
-path_to_deeprvat='/path/to/deeprvat/repo'
+deeprvat_dir='/path/to/deeprvat/repo'
+deeprvat_analysis_dir='/path/to/deeprvat_analysis/repo'
 
 mkdir experiment_dir
 cd ./experiment_dir
 
 mkdir -p ./base
-ln -s $path_to_deeprvat/example/* ./base/
+ln -s $deeprvat_dir/example/* ./base/
 
 for fold in $(seq 0 $(($FOLDS - 1)) ); do
     mkdir -p ./fold_$fold
-    ln -s $path_to_deeprvat/example/* ./fold_$fold/
+    ln -s $deeprvat_dir/example/* ./fold_$fold/
 done
 
 # RUN BASE EXP SEED GENE SELECTION ------------------------------------------
 cd ./base
-snakemake -j 1 --snakefile $path_to_deeprvat/pipelines/training_association_testing.snakefile all_config
+snakemake -j 1 --snakefile $deeprvat_dir/pipelines/training_association_testing.snakefile all_config
 cd ../
 
 # RUN SEED GENE FOLD SELECTION ----------------------------------------------
 # Run seed_gene_selection to yield seed_gene.parquet files in each FOLD dir
-python ~/deeprvat-analysis-main/deeprvat-analysis/seed_gene_sensitivity/seed_gene_selection.py --folds $FOLDS --downsample_percent 0.1 --min_keep_percent 0.5
+python $deeprvat_analysis_dir/seed_gene_sensitivity/seed_gene_selection.py --folds $FOLDS --downsample_percent 0.1 --min_keep_percent 0.5
 
 # RUN ASSOCIATION TESTING PIPELINE ON EACH SEED GENE FOLD -------------------
 for fold in $(seq 0 $(($FOLDS - 1)) ); do
     cd ./fold_$fold
-    snakemake -j 1 --snakefile $path_to_deeprvat/pipelines/training_association_testing.snakefile -n
+    snakemake -j 1 --snakefile $deeprvat_dir/pipelines/training_association_testing.snakefile -n
     cd ../
 done
